3ad902c177e475077d79f067361058d704a46429
Joseph Cashell <56300631+joepcash@users.noreply.github.com>
2020-05-08 13:11:22 +0100

    Began using tensorflow.

---
+++ src/models/tf_lr.py
@@ -0,0 +1,27 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv(r'../../data/interim/mood_features.csv')
+activities = list(df.iloc[:,1:].columns.values)
+dataset = df.copy()
+
+train_dataset = dataset.sample(frac=0.8,random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+train_labels = train_dataset.pop('mood')
+test_labels = test_dataset.pop('mood')
+
+estimator = tf.estimator.LinearRegressor(feature_columns=train_dataset)
+
+
+def get_input_fn(num_epochs=None, n_batch=200, shuffle=True):
+    return tf.estimator.inputs.pandas_input_fn(
+        x=pd.DataFrame({k: train_dataset[k].values for k in activities}),
+        y=pd.Series(train_labels.values),
+        batch_size=n_batch,
+        num_epochs=num_epochs,
+        shuffle=shuffle)
+
+estimator.train(input_fn=get_input_fn(num_epochs=None, n_batch = 128, shuffle=False),steps=1000)
