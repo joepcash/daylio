0bddc66b84b7effd536dbebd601e4d58d86ebcd2
Joseph Cashell <56300631+joepcash@users.noreply.github.com>
2020-05-13 18:33:15 +0100

    Datetime col now references midpoint of each time period.

--- src/features/make_time_since_activity_features.py
+++ src/features/make_time_since_activity_features.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import tabulate as tabulate
+import numpy as np
 
 df = pd.read_csv(r'../../data/interim/activity_occurrences.csv')
 
@@ -7,13 +8,16 @@ df = df.fillna(0)
 df.iloc[:,5:] = df.iloc[:,5:].astype(bool)
 df['datetime'] = pd.to_datetime(df['datetime'])
 df['index'] = df['datetime']
-df['time'] = df['datetime'].dt.time
-df['date'] = df['datetime'].dt.date
 df = df.set_index('index')
 df = df.between_time('08:00', '00:00')
-df['rank'] = df.groupby('date')['datetime'].rank(ascending=True)
-df['diff'] = df['datetime'].diff(periods=-1)
-df['newtime'] = df['datetime'] - df['diff']/2
-df.loc[df['rank'] == 1, 'newtime'] = pd.Timestamp()
+df['rank'] = df.groupby(df['datetime'].dt.date)['datetime'].rank(ascending=True)
+df['datetime'] = np.where(df['rank'] == 1,
+                         df['datetime'] -
+                         (df['datetime'] -
+                          (pd.to_datetime(df['date']) + pd.Timedelta(hours=8)))/2
+                         , df['datetime'] - (df['datetime'].diff(periods=-1))/2)
+df.drop(columns=['date','rank','activities','weekday'], axis=1, inplace=True)
+df.reset_index()
+print(df.info())
 
-df.to_csv(r'../../data/processed/time_since_activity_features.csv', index='False')
+df.to_csv(r'../../data/processed/time_since_activity_features.csv', index=False)
